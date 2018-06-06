ca04f80993127c28c81f47ad9fdc5849ac0b5ef9
Ilya Dzeraziak <ilyadzeraziak@Ilyas-MacBook-Air.local>
2018-06-06 21:15:08 +0300

    Added Party model

--- src/DLL/models/Party.cs
+++ src/DLL/models/Party.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace DLL.models
 {
     public class Party
     {
-        public int Id {get; set;}
-        public string Name {get; set;}
-        public string Description {get; set;}
-        public ICollection<User> Partisipants {get; set;}
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public DataTime TimeStart {get;set;}
+        public DataTime TimeEnd {get;set;}
+        public string Description {get;set;}
+        public ICollection<User> Partisipants {get;set;}
     }
 }
