50b856ece0fa39c05a6afc62f9e21bf55d3cda65
Ilya Dzeraziak <ilyadzeraziak@Ilyas-MacBook-Air.local>
2018-06-08 18:06:36 +0300

    Added id field to a rewiew

--- src/DLL/models/Rewiew.cs
+++ src/DLL/models/Rewiew.cs
@@ -2,7 +2,8 @@ namespace DLL.Models
 {
     public class Rewiew
     {
-        public string Name{get; set;}
-        public string Description{get; set;}
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string Description {get; set;}
     }
 }
