c3286a26749a7b656738ccccf070e66a51772000
Ilya Dzeraziak <ilyadzeraziak@Ilyas-MacBook-Air.local>
2018-06-08 16:44:52 +0300

    Added a skeleton of User repository

---
+++ src/DLL/Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+using DLL.Interfaces;
+using System;
+
+namespace DLL.Repository
+{
+    public class UserRepository : IUserRepository, IDisposable
+    {
+        public IEnumerable GetAllUsers()
+        {
+        
+        }
+        public User GetUserById(int id)
+        {
+
+        }
+        public void InsertUser(User user)
+        {
+
+        }
+        public void UpdateUser(User user)
+        {
+
+        }
+        public void DeleteUser(User user)
+        {
+            
+        }
+        public void Save()
+        {
+
+        }
+    }
+}
